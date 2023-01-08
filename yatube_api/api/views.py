3a0b52983d11681e5d796678b7011768a42a412c
SemenovY <info@juriys.ru>
2023-01-08 14:38:53 +0300

    fix bugs after review02, pytest ok

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,7 +2,6 @@
 from django.shortcuts import get_object_or_404
 
 from rest_framework import filters, mixins, viewsets
-from rest_framework.exceptions import PermissionDenied
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 
